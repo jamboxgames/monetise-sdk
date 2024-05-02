7018807a493710970283bbe8b378bfe6887ad29b
AJITH\welcome <ajiht.r@jambox.games>
2024-05-02 10:41:39 +0530

    version 1.0.8

--- Jambox/build.gradle.kts
+++ Jambox/build.gradle.kts
@@ -68,11 +68,11 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.github.jamboxgames"
                 artifactId = "monetise-sdk"
-                version = "1.0.4"
+                version = "1.0.8"
 
                 from(components["release"])
 
-                //com.github.jamboxgames:monetise-sdk:1.0.4
+                //com.github.jamboxgames:monetise-sdk:1.0.8
             }
         }
     }
